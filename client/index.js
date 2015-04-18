8057858a538c4abdb5062a3e7fe003590c0df255
Christian Richard <lifepupil@gmail.com>
2015-04-18 14:25:06 -0700

    UI update

--- client/index.js
+++ client/index.js
@@ -5,6 +5,6 @@ var root;
 $(document).ready(init);
 
 function init(){
-  root = new Firebase('https://zelda-cdr.firebaseio.com/');
+  root = new Firebase('https://battleship-cdr.firebaseio.com/');
 
 }
